25e1ade3e76e43bf0a8378deef91ea3126fc4c1f
Rickags <ricardogoncalvescontato@gmail.com>
2020-11-29 10:52:32 -0300

    Subindo projeto

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo.list.module';
+import { PhotoModule } from './photo/photo.module';
+
+@NgModule({    
+    imports: [ 
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,        
+        CommonModule
+    ]
+})
+
+export class PhotosModule{}
